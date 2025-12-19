1485fc12786ec8ab484a770c57df938b85f4a10b
Scott Dyrness <scottdyrness@gmail.com>
2025-12-18 18:24:15 -0800

    Fixed errors for second submission

--- app.js
+++ app.js
@@ -4,13 +4,24 @@ console.log("Hello World!\n==========\n");
 console.log("EXERCISE 1:\n==========\n");
 
 function printOdds(count){
-    for(let i = -count; i<count;i++)
-        if(i % 2 != 0){
-            console.log(i);
+        if (count > 0){
+            for(let i = 0; i < count; i++){
+                if(i % 2 != 0){
+                    console.log(i);
+                }
+            }
+        }
+        else{
+            for(let i = count; i < 0; i++){
+                if(i % 2 != 0){
+                    console.log(i);
+                }
+            }
         }
     }
     
     printOdds(10);
+    printOdds(-10);
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
@@ -36,10 +47,10 @@ console.log("EXERCISE 3:\n==========\n");
 
 function whichQuadrant(x,y){
     
-    if(x == 0){
+    if(x == 0 && y != 0){
         console.log(`(${x},${y}) lies on the Y axis.`);
     }
-    if( y == 0){
+    if( y == 0 && x != 0){
         console.log(`(${x},${y}) lies on the X axis.`);
     }
     if(x > 0 && y > 0){
@@ -54,6 +65,9 @@ function whichQuadrant(x,y){
     if(x < 0 && y < 0){
         console.log(`(${x},${y}) is in quadrant 3.`);
     }
+    if(x == 0 && y == 0){
+        console.log(`(${x},${y}) is at the origin.`)
+    }
 }
 
 whichQuadrant(1,0);
@@ -62,13 +76,14 @@ whichQuadrant(-1,1);
 whichQuadrant(1,-1);
 whichQuadrant(1,1);
 whichQuadrant(-1,-1);
+whichQuadrant(0,0);
 
 // Exercise 4 Section
 console.log("EXERCISE 4:\n==========\n");
 
 function typeOfTriangle(a,b,c){
     let numbers = [a,b,c];
-    let sorted = numbers.sort();
+    let sorted = numbers.sort((x,y) => x-y);
 
     let x = sorted[0];
     let y = sorted[1];
@@ -81,9 +96,9 @@ function typeOfTriangle(a,b,c){
         if( x == y && y !=z || x!= y && y == z){
             console.log(`(${numbers}) This is an isoceles triangle.`)
         }
-        if(x < y && y !=z)[
+        if(x < y && y !=z){
             console.log(`(${numbers}) This is a scalene triangle.`)
-        ]
+        }
     }
     else{
         console.log(`(${numbers}) This is not a triangle.`)
@@ -96,6 +111,9 @@ typeOfTriangle(3,3,3)
 typeOfTriangle(4,5,6)
 typeOfTriangle(2,3,3)
 typeOfTriangle(5,4,9)
+typeOfTriangle(0,1,3)
+typeOfTriangle(0,0,3)
+typeOfTriangle(0,0,0)
 
 // Exercise 5 Section
 console.log("EXERCISE 5:\n==========\n");
@@ -116,6 +134,7 @@ let planMaxRound = Math.round(planMaxAtDay * 100)/100;
 let planDailyRound = Math.round(planDailyAverage * 100)/100;
 let userRound = Math.round(userDailyAverage * 100)/100;
 let difference = usage - planMaxRound;
+let estimate = userRound * 30;
 
 
 if(planDailyAverage > userDailyAverage){
@@ -123,6 +142,7 @@ if(planDailyAverage > userDailyAverage){
     console.log(`At day ${day}, your plans average should total to ${planMaxRound}gb. This is ${-difference}gb under.`)
     console.log(`your daily average usage is ${userRound}gb.`)
     console.log(`You are under the max daily average of ${planDailyRound}gb`)
+    console.log(`At this rate you will use ${estimate}gb by the end of the 30 days.`)
     console.log(`You can downgrade your plan to avoid wasting data.`)
 }
 
@@ -131,6 +151,7 @@ if(planDailyAverage < userDailyAverage){
     console.log(`At day ${day}, your plans average should total to ${planMaxRound}gb, This is ${difference}gb over.`)
     console.log(`your daily average usage is ${userRound}gb.`)
     console.log(`You are over the max daily average of ${planDailyRound}gb.`)
+    console.log(`At this rate you will use ${estimate}gb by the end of the 30 days.`)
     console.log(`You should upgrade your plan to avoid running out of data.`)
 }
 
@@ -139,6 +160,7 @@ if(planDailyAverage == userDailyAverage){
     console.log(`At day ${day}, your plans average should total to ${planMaxRound}gb.`)
     console.log(`your daily average usage is ${userRound}gb.`)
     console.log(`You are at the max daily average of ${planDailyRound}gb.`)
+    console.log(`At this rate you will use ${estimate}gb by the end of the 30 days.`)
     console.log(`consider upgrading plan to avoid going over.`)
 }
 }
